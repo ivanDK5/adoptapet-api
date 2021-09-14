5bd45e1773bed4a44d20f6737a1a4522cf2f7471
ivanDK5 <ing.iglds@gmail.com>
2021-09-13 19:21:51 -0500

    feat:Added models to main js file

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//cdatabase connection
+
+const mongoose= require('mongoose');
+
+mongoose.connect('mongodb+srv://ivanDK5:Esteta10@cluster0.f8vdq.mongodb.net/adoptapet?retryWrites=true&w=majority');
+
+mongoose.set('debug',true);
+require('./models/Usuario');
+require('./models/Mascota');
+require('./models/Solicitud');
+
 app.use('/v1/',require('./routes'));
 
 const PORT = 4001;
