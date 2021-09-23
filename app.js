81385a3f4f287d3c49d916f9d3bac82919c2c082
ivanDK5 <ing.iglds@gmail.com>
2021-09-22 20:09:57 -0500

    feat:Added env variables

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.use(bodyParser.json());
 
 //cdatabase connection
 
-mongoose.connect('mongodb+srv://ivanDK5:Esteta10@cluster0.f8vdq.mongodb.net/adoptapet?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGODB_URI);
 mongoose.set('debug',true);
 
 
 app.use('/v1/',require('./routes'));
 
 const PORT = 4001;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${procces.env.PORT}`);
 });
