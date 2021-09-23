3ba73908a7772ca48edd58a4f7c6ac41ec63f1f6
ivanDK5 <ing.iglds@gmail.com>
2021-09-22 20:05:18 -0500

    feat:Added imports

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+const mongoose= require('mongoose');
+require('./models/Usuario');
+require('./models/Mascota');
+require('./models/Solicitud');
+require('./config/passport')
 const express = require('express');
 const app = express();
 const bodyParser=require('body-parser');
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //cdatabase connection
 
-const mongoose= require('mongoose');
-
 mongoose.connect('mongodb+srv://ivanDK5:Esteta10@cluster0.f8vdq.mongodb.net/adoptapet?retryWrites=true&w=majority');
-
 mongoose.set('debug',true);
-require('./models/Usuario');
-require('./models/Mascota');
-require('./models/Solicitud');
+
 
 app.use('/v1/',require('./routes'));
 
